26e35877c3a4bffa60939057e8b5b24085183290
shifty189 <shifty189@gmail.com>
2025-08-01 21:36:22 -0400

    Add files via upload

---
+++ Classes.py
@@ -0,0 +1,5 @@
+class Person:
+    def __init__(self, name, birthday, cstart):
+        self.name = name
+        self.birthday = birthday
+        self.Last_Cycle_start = cstart
---
+++ Tracker.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+import calendar
+
+print(calendar.calendar(2025))
+
+buildDate = "7/30/2025"
+version = "0.01"
+
+"""
+the goal here is build a application that will track the menstrual cycle of a number of women. the primary purpose
+is to predict when the next menstrual cycle will begin and when the current one will end
+"""
+
+"""
+Stage 1
+have a user selectiong and creation section. this first area is where you can either create a new user to go into
+and existing user. no security is needed here
+"""
+
+"""
+Stage 1b
+if its seclected to create a new user, create a new account and add them to the user index
+"""
+
+"""
+Stage 2
+Open the users data and display a card link display to show when the last cycle ended and a prediction of when the 
+next cycle will begin
+"""
+
+"""
+Stage 3
+at some point we need to evaluate the cycles and make the predictions of the next cycle
+"""
