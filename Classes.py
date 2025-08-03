acbaaf17039d660aa9e86fc8b411d493556517bd
shifty189 <shifty189@gmail.com>
2025-08-03 12:58:22 -0400

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -1,5 +1,8 @@
+import tkcalendar
+
 class Person:
-    def __init__(self, name, birthday, cstart):
+    def __init__(self, name, birthday, cstart, Frame):
         self.name = name
         self.birthday = birthday
         self.Last_Cycle_start = cstart
+        self.calendar = tkcalendar.Calendar(Frame)
